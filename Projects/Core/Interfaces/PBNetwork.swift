3fbf6473715af2555d3797a3685dc5ca8a4b8c57
Hohyeon Moon <hohyeonmoon@gmail.com>
2023-04-29 17:23:35 +0900

    Network 프로토콜 추가

---
+++ Projects/Core/Interfaces/PBNetwork.swift
@@ -0,0 +1,42 @@
+//
+//  Network.swift
+//  Core
+//
+//  Created by Hohyeon Moon on 2023/04/29.
+//
+
+import Foundation
+import Moya
+import RxMoya
+
+protocol PBNetwork {
+  func request<T: Decodable>(
+    _ target: TargetType,
+    completion: @escaping (Result<T, Error>) -> Void
+  )
+}
+
+class PBNetworkImpl: PBNetwork {
+  private let provider = MoyaProvider<PBAPI>()
+
+  func request<T: Decodable>(
+    _ target: TargetType,
+    completion: @escaping (Result<T, Error>) -> Void
+  ) {
+    provider.rx.request(.createUser(name: "Hohyeon", email: "hohyeonmoon@gmail.com"))
+      .subscribe { result in
+      switch result {
+      case let .success(response):
+        do {
+          let decoder = JSONDecoder()
+          let data = try decoder.decode(T.self, from: response.data)
+          completion(.success(data))
+        } catch {
+          completion(.failure(error))
+        }
+      case let .failure(error):
+        completion(.failure(error))
+      }
+    }
+  }
+}
