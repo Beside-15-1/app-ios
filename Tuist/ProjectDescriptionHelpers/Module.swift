ca704f27831309d328d1e3d5db70df9697593683
sonny-park <sonny.park@daangnpay.com>
2023-05-04 00:40:16 +0900

    feat: Module 정의 및 모듈별 TargetDependency함수 작성

---
+++ Tuist/ProjectDescriptionHelpers/Module.swift
@@ -0,0 +1,19 @@
+//
+//  ProjectName.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by cheonsong on 2022/09/02.
+//
+
+import ProjectDescription
+
+public enum Module: String, CaseIterable {
+  case App = "Joosum"
+  case Domain
+  case Presentation
+  case DesignSystem
+}
+
+public enum CoreModule: String, CaseIterable {
+  case Networking
+}
---
+++ Tuist/ProjectDescriptionHelpers/TargetDependency+Module.swift
@@ -0,0 +1,50 @@
+//
+//  TargetDependency+Module.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by 박천송 on 2023/05/04.
+//
+
+import ProjectDescription
+
+// MARK: Presentation
+
+extension TargetDependency {
+  public static func presentation()-> TargetDependency {
+    .project(target: "Presentation", path: .relativeToRoot("Projects/Presentation"))
+  }
+}
+
+// MARK: DesignSystem
+
+extension TargetDependency {
+  public static func designSystem()-> TargetDependency {
+    .project(target: "DesignSystem", path: .relativeToRoot("Projects/DesignSystem"))
+  }
+}
+
+
+// MARK: Domain
+
+extension TargetDependency {
+  public static func domain()-> TargetDependency {
+    .project(target: "Domain", path: .relativeToRoot("Projects/Domain"))
+  }
+
+  public static func data()-> TargetDependency {
+    .project(target: "Data", path: .relativeToRoot("Projects/Domain"))
+  }
+}
+
+// MARK: Core
+
+extension TargetDependency {
+  public static func core(impl module: CoreModule)-> TargetDependency {
+    .project(target: module.rawValue, path: .relativeToRoot("Projects/Core/" + module.rawValue))
+  }
+
+  public static func core(interface module: CoreModule)-> TargetDependency {
+    .project(target: module.rawValue + "Interface", path: .relativeToRoot("Projects/Core/" + module.rawValue))
+  }
+}
+
