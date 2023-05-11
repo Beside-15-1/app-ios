d11fff3320f97ffb6f73de34658ea3714dd9bddd
sonny-park <sonny.park@daangnpay.com>
2023-05-11 11:47:05 +0900

    JOO-16 feat: EvnetNameBuilder 구현

---
+++ Projects/Core/JSAnalytics/Interfaces/EventBuilder/JSAnalyticsActionType.swift
@@ -0,0 +1,15 @@
+//
+//  JSAnalyticsActionType.swift
+//  JSAnalyticsInterface
+//
+//  Created by 박천송 on 2023/05/11.
+//
+
+import Foundation
+
+public enum JSAnalyticsActionType: String {
+  case click
+  case longPress
+  case shown
+  case swipe
+}
---
+++ Projects/Presentation/Sources/Analytics/LoginEvent.swift
@@ -0,0 +1,8 @@
+//
+//  LoginEvent.swift
+//  Presentation
+//
+//  Created by 박천송 on 2023/05/11.
+//
+
+import Foundation
