aa0224202e7e27f5bfc4b1f1a0cf88526f7db250
Hohyeon Moon <hohyeonmoon@gmail.com>
2023-05-21 01:56:52 +0900

    Make project중 발생한 PBLog 에러 수정

---
+++ Projects/Core/PBLog/Project.swift
@@ -0,0 +1,58 @@
+//
+//  Project.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by Hohyeon Moon on 2023/05/13.
+//
+
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project(
+  name: CoreModule.PBLog.rawValue,
+  targets: [
+    Target(
+      name: "\(CoreModule.PBLog.rawValue)Interface",
+      platform: .iOS,
+      product: .staticFramework,
+      bundleId: Project.bundleID + ".\(CoreModule.PBLog.rawValue)Interface".lowercased(),
+      deploymentTarget: .iOS(targetVersion: Project.iosVersion, devices: [.iphone, .ipad]),
+      infoPlist: .file(path: .relativeToRoot("Supporting Files/Info.plist")),
+      sources: ["Interfaces/**"],
+      scripts: [.SwiftFormatString],
+      dependencies: [
+        .external(dependency: .RxMoya)
+      ]
+    ),
+    Target(
+      name: CoreModule.PBLog.rawValue,
+      platform: .iOS,
+      product: .staticFramework,
+      bundleId: Project.bundleID + ".\(CoreModule.PBLog.rawValue)".lowercased(),
+      deploymentTarget: .iOS(targetVersion: Project.iosVersion, devices: [.iphone, .ipad]),
+      infoPlist: .file(path: .relativeToRoot("Supporting Files/Info.plist")),
+      sources: ["Sources/**"],
+      scripts: [.SwiftFormatString],
+      dependencies: [
+        .target(name: "\(CoreModule.PBLog.rawValue)Interface"),
+        // External
+        .external(dependency: .RxMoya),
+        .external(dependency: .Swinject)
+      ]
+    ),
+    Target(
+      name: "\(CoreModule.PBLog.rawValue)Tests",
+      platform: .iOS,
+      product: .unitTests,
+      bundleId: Project.bundleID + ".\(CoreModule.PBLog.rawValue)Tests".lowercased(),
+      deploymentTarget: .iOS(targetVersion: Project.iosVersion, devices: [.iphone, .ipad]),
+      infoPlist: .file(path: .relativeToRoot("Supporting Files/Info.plist")),
+      sources: "Tests/**",
+      scripts: [.SwiftFormatString],
+      dependencies: [
+        .target(name: "\(CoreModule.PBLog.rawValue)"),
+        .target(name: "\(CoreModule.PBLog.rawValue)Interface")
+      ]
+    )
+  ]
+)
